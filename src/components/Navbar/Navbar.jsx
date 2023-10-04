31d3dfaaa9e30ccd3c83ef167d7a4fa51b362afc
annamowinska <mowinska.ania@gmail.com>
2023-10-04 15:33:45 +0200

    mobil_version

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,31 @@ import React, { useState, useEffect } from "react";
 export const Navbar = () => {
     const [isNavbarFixed, setIsNavbarFixed] = useState(false);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [showScrollToTopButton, setShowScrollToTopButton] = useState(false);
+
+    function scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
+
+    useEffect(() => {
+      function handleScroll() {
+        if (window.scrollY > 200) {
+          setShowScrollToTopButton(true);
+        } else {
+          setShowScrollToTopButton(false);
+        }
+      }
+  
+      window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
     const handleScroll = () => {
       if (window.scrollY > 0) {
@@ -31,6 +56,10 @@ export const Navbar = () => {
     
     const isMobile = windowWidth <= 767;
 
+    const toggleMobileMenu = () => {
+      setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     const navbarClassName = isNavbarFixed ? `${css.box} ${css.fixed}` : css.box;
 
     const scrollToSection = (id) => {
@@ -49,17 +78,55 @@ export const Navbar = () => {
         }
       };
 
-  return (
-    <nav className={isMobile ? css.hidden : navbarClassName}>
-        <ul className={css.list}>
-        {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
-          <li className={css.item} key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`} className={css.link} onClick={(e) => { e.preventDefault();
-            scrollToSection(item);}}>{item}</a>
-          </li>
-        ))}
-        </ul>
-      </nav>
-  );
-};
+      return (
+        <nav className={isMobile ? css.mobileNavbar : navbarClassName}>
+          {isMobile && !isMobileMenuOpen && (
+            <div className={css.mobileMenuIcon} onClick={toggleMobileMenu}>
+              {/* Burger (menu) */}
+              <div className={css.bar}></div>
+              <div className={css.bar}></div>
+              <div className={css.bar}></div>
+            </div>
+          )}
+          {isMobileMenuOpen && isMobile && (
+            <div className={css.mobileMenuList}>
+              <div className={css.closeIcon} onClick={toggleMobileMenu}>
+                <span className={css.closeBar}>X</span>
+              </div>
+              <ul className={css.mobileMenuLinks}>
+                {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
+                  <li className={css.mobileMenuItem} key={`mobile-link-${item}`}>
+                    <a
+                      href={`#${item}`}
+                      className={css.mobileMenuLink}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        scrollToSection(item);
+                        toggleMobileMenu();
+                      }}
+                    >
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {!isMobile && (
+            <ul className={css.list}>
+              {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
+                <li className={css.item} key={`link-${item}`}>
+                  <div />
+                  <a href={`#${item}`} className={css.link} onClick={(e) => { e.preventDefault(); scrollToSection(item); }}>{item}</a>
+                </li>
+              ))}
+            </ul>
+          )}
+          {showScrollToTopButton && (
+        <div id="scroll-to-top-button" onClick={scrollToTop} className={css.scroll}>
+          <span className={css.arrow}>↑</span>
+        </div>
+      )}
+        </nav>
+      );
+    };
