ecc42954679ea8df16a8632cc94c13e4459d4c76
annamowinska <mowinska.ania@gmail.com>
2023-10-17 16:47:43 +0200

    contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,19 @@
 // import PropTypes from 'prop-types';
 import css from './Contact.module.css';
 import cvFile from '../Data/cv.pdf'
+import contactData from '../Data/contact';
+import { IconContext } from 'react-icons';
+import { AiOutlineMail, AiOutlinePhone, AiOutlineGithub } from 'react-icons/ai';
+import { BiLogoLinkedin } from 'react-icons/bi';
 
-export const Contact = () => {
+const iconComponents = {
+  AiOutlineMail: AiOutlineMail,
+  AiOutlinePhone: AiOutlinePhone,
+  AiOutlineGithub: AiOutlineGithub,
+  BiLogoLinkedin: BiLogoLinkedin,
+};
+
+export const Contact = ({ contact }) => {
   const handleDownload = () => {
     window.open(cvFile, '_blank');
   };
@@ -12,7 +23,18 @@ export const Contact = () => {
       <div className={css.header}>
         <h2 className={css.name}>Contact</h2>
       </div>
-      <p>email: mowinska.ania@gmail.com</p>
+      <div className={css.contact}>
+        {contactData.map(item => {
+          const IconComponent = iconComponents[item.icon];
+          return (
+            <div className={css.item} key={item.contact}>
+              <IconContext.Provider value={{ className: css.icon }}>
+                {IconComponent && <IconComponent />}
+              </IconContext.Provider>
+            </div>
+          );
+        })}
+        </div>
       <button className={css.button} onClick={handleDownload}>Download CV</button>
     </div>
   );
